2d615567692b7d17db2eb5e9ea1a7beadd94a986
yetiblue <thempver@gmail.com>
2021-05-21 17:06:34 -0400

    add: tag chips + working on toolbar

--- portfolio/pages/testcards.vue
+++ portfolio/pages/testcards.vue
@@ -1,12 +1,31 @@
 <template>
   <div id="app">
     <v-app>
-      <v-tabs style="height:10vh">
+      <v-toolbar>
+        <v-toolbar-title>Responsive Template Site</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn v-for="item in menu" :key="item.icon" :to="item.link" flat>{{
+            item.title
+          }}</v-btn>
+        </v-toolbar-items>
+        <v-menu class="hidden-md-and-up">
+          <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+          <v-list>
+            <v-list-tile v-for="item in menu" :key="item.icon">
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
+      </v-toolbar>
+      <!-- <v-tabs show-arrows style="height:10vh" s>
         <v-tab>Item One</v-tab>
         <v-tab>Item Two</v-tab>
         <v-tab>Item Three</v-tab>
         <v-tab>Item Four</v-tab>
-      </v-tabs>
+      </v-tabs> -->
       <v-container fluid pa-0>
         <v-row align="center" justify="center" style="height:40vh">
           <v-col
@@ -30,17 +49,23 @@
         <v-row align="center" style="height:60vh;">
           <v-col cols="12" lg="6" md="6">
             <v-card class="bg2" style="height:50vh">
-              <h1
+              <h2
                 :class="[
-                  $vuetify.breakpoint.mdAndUp ? 'display-4' : 'display-2'
+                  $vuetify.breakpoint.mdAndUp ? 'text-h4' : 'subtitle-1'
                 ]"
+                style="padding-top:10px; padding-left: 5px; padding-bottom: 10px"
+              >
+                <v-chip :small="$vuetify.breakpoint.mobile" color="yellow">
+                  Tag
+                </v-chip>
+              </h2>
+              <h1
+                :class="[$vuetify.breakpoint.mdAndUp ? 'text-h1' : 'display-2']"
               >
                 Title
               </h1>
               <h1
-                :class="[
-                  $vuetify.breakpoint.mdAndUp ? 'display-3' : 'display-1'
-                ]"
+                :class="[$vuetify.breakpoint.mdAndUp ? 'text-h2' : 'text-h4']"
               >
                 Subtitle
               </h1>
@@ -48,6 +73,16 @@
           </v-col>
           <v-col cols="12" lg="6" md="6">
             <v-card class="bg3 white--text" style="height:50vh">
+              <h2
+                :class="[
+                  $vuetify.breakpoint.mdAndUp ? 'text-h4' : 'subtitle-1'
+                ]"
+                style="padding-top:10px; padding-left: 5px; padding-bottom: 10px"
+              >
+                <v-chip :small="$vuetify.breakpoint.mobile" color="yellow">
+                  Tag
+                </v-chip>
+              </h2>
               <h1
                 :class="[
                   $vuetify.breakpoint.mdAndUp ? 'display-4' : 'display-2'
@@ -98,3 +133,22 @@
   background-size: cover;
 }
 </style>
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      menu: [
+        { icon: "home", title: "Link A" },
+        { icon: "info", title: "Link B" },
+        { icon: "warning", title: "Link C" }
+      ]
+    };
+  },
+  methods: {
+    menuItems() {
+      return this.menu;
+    }
+  }
+};
+</script>
