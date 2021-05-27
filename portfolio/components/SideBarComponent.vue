f154463f2bccd0d717618dc5fe10f1ce89f41b16
yetiblue <thempver@gmail.com>
2021-05-27 14:07:36 -0400

    add: responsiveness to all screen sizes for multi grid page

--- portfolio/components/SideBarComponent.vue
+++ portfolio/components/SideBarComponent.vue
@@ -9,59 +9,53 @@
               height="300px"
               width="300px"
               src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
-            >
-            </v-img>
+            ></v-img>
           </div>
-          <div v-else style="margin-left:22vw">
+          <div v-else>
             <v-img
+              class="mx-auto"
               justify="center"
               height="300px"
               width="300px"
               src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
-            >
-            </v-img>
+            ></v-img>
           </div>
 
-          <v-card-title>
-            Name
-          </v-card-title>
+          <v-card-title>Name</v-card-title>
           <v-card-text>
             I am a very cool kid who likes to fill out their information like a
             good kid i dont know why you would ever have it this small but ok
             whatever i hope that this is ok and that it fits
           </v-card-text>
           <v-card-actions>
-            <v-icon class="ml-6 ml-sm-2 ml-lg-0">mdi-instagram</v-icon
-            ><v-icon class="ml-4">mdi-youtube</v-icon>
+            <v-icon class="ml-6 ml-sm-2 ml-lg-0">mdi-instagram</v-icon>
+            <v-icon class="ml-4">mdi-youtube</v-icon>
 
             <v-icon class="ml-4">mdi-vimeo</v-icon>
-            <v-icon class="ml-4">mdi-linkedin</v-icon></v-card-actions
-          >
-        </v-card></v-container
-      >
+            <v-icon class="ml-4">mdi-linkedin</v-icon>
+          </v-card-actions>
+        </v-card>
+      </v-container>
     </div>
 
     <div v-else>
       <div :style="{ height: sideHeight }">
         <!-- <div v-if="!mobile" > -->
         <v-card class="pa-8" flat>
-          <v-img src="https://cdn.vuetifyjs.com/images/cards/halcyon.png">
-          </v-img>
-          <v-card-title>
-            Name
-          </v-card-title>
+          <v-img src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"></v-img>
+          <v-card-title>Name</v-card-title>
           <v-card-text>
             I am a very cool kid who likes to fill out their information like a
             good kid i dont know why you would ever have it this small but ok
             whatever i hope that this is ok and that it fits
           </v-card-text>
           <v-card-actions>
-            <v-icon class="ml-2 ml-lg-0">mdi-instagram</v-icon
-            ><v-icon class="ml-4">mdi-youtube</v-icon>
+            <v-icon class="ml-2 ml-lg-0">mdi-instagram</v-icon>
+            <v-icon class="ml-4">mdi-youtube</v-icon>
 
             <v-icon class="ml-4">mdi-vimeo</v-icon>
-            <v-icon class="ml-4">mdi-linkedin</v-icon></v-card-actions
-          >
+            <v-icon class="ml-4">mdi-linkedin</v-icon>
+          </v-card-actions>
         </v-card>
       </div>
     </div>
--- portfolio/pages/multitables.vue
+++ portfolio/pages/multitables.vue
@@ -5,12 +5,11 @@
       <v-row flat justify="end">
         <SideBarComponent :sideHeight="sideHeight" />
 
-        <v-col class="pt-11 mt-sm-n16 mt-lg-0" cols="9" sm="12" lg="9"
-          ><v-col
-            ><v-card flat class="ml-n6 text-sm-center text-lg-left"
-              ><v-card-title :class="[forIpad ? 'justify-center' : 'none']">
-                PIRATES OF THE CARRIBEAN {{ displayGridCount }} </v-card-title
-              ><v-card-subtitle> Date Posted </v-card-subtitle>
+        <v-col class="pt-11 mt-sm-n16 mt-lg-0" cols="12" sm="12" lg="9">
+          <v-col>
+            <v-card flat class="text-center text-lg-left">
+              <v-card-title :class="[forIpad ? 'justify-center' : 'none']">PIRATES OF THE CARRIBEAN</v-card-title>
+              <v-card-subtitle>Date Posted</v-card-subtitle>
               <v-container>
                 <v-card-text class="ml-lg-n4 mt-lg-n6">
                   fdhsiaudsoxjvdfsdfdsfds fdhsiaudsoxjvdfsdfdsfds
@@ -19,23 +18,19 @@
                   fdhsiaudsoxjvdfsdfdsfds fdhsiaudsoxjvdfsdfdsfds
                 </v-card-text>
               </v-container>
-            </v-card></v-col
-          >
+            </v-card>
+          </v-col>
         </v-col>
-        <v-spacer> </v-spacer>
+        <v-spacer></v-spacer>
       </v-row>
       <div>
         <v-row class="pb-lg-16 mb-lg-16" justify="end" align="start">
-          <v-col cols="10"> Applicants </v-col>
-          <v-col
-            class="pb-lg-16"
-            cols="12"
-            sm="12"
-            md="12"
-            lg="10"
-            justify="end"
-          >
-            <v-row class="mb-lg-n16" justify="start">
+          <v-col cols="8" sm="10" lg="9" class="pl-8 pl-sm-14 pl-lg-4">Applicants</v-col>
+          <v-col class="pr-16 pr-lg-0" cols="4" sm="2" lg="1">
+            <v-btn text>View All</v-btn>
+          </v-col>
+          <v-col class="pb-lg-16" cols="12" sm="12" md="12" lg="10" justify="end">
+            <v-row class="mb-lg-n16 pl-4 pr-4 pl-sm-10 pl-lg-0 pr-sm-10 pr-lg-8" justify="start">
               <v-col
                 justify="end"
                 v-for="n in displayGridCount"
@@ -55,54 +50,54 @@
                     height="25vh"
                     src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
                   >
-                    <v-card-title align="end"> Category Name</v-card-title>
-                    <v-card-subtitle> Description of gallery</v-card-subtitle
-                    ><v-card-text> Date information</v-card-text>
-                    <v-row align="center" justify="end"> </v-row>
+                    <v-card-title align="end">Category Name</v-card-title>
+                    <v-card-subtitle>Description of gallery</v-card-subtitle>
+                    <v-card-text>Date information</v-card-text>
+                    <v-row align="center" justify="end"></v-row>
                   </v-img>
                 </v-card>
               </v-col>
             </v-row>
           </v-col>
-          <v-col cols="12"> Open Positions </v-col>
+          <v-col cols="8" sm="10" lg="9" class="pl-8 pl-sm-14 pl-lg-4">Applicants</v-col>
+          <v-col class="pr-16 pr-lg-0" cols="4" sm="2" lg="1">
+            <v-btn text>View All</v-btn>
+          </v-col>
           <v-col class="pb-lg-16" cols="12" lg="10" justify="end">
-            <v-row class="mb-lg-n16" justify="start">
-              <v-col
-                justify="end"
-                v-for="n in positionsGridCount"
-                :key="n"
-                cols="12"
-                sm="6"
-                lg="4"
-              >
+            <v-row class="mb-lg-n16 pl-4 pl-sm-10 pl-lg-0 pr-4 pr-sm-10 pr-lg-8" justify="start">
+              <v-col justify="end" v-for="n in positionsGridCount" :key="n" cols="12" sm="6" lg="4">
                 <v-card style="height:25vh" outlined title>
                   <v-img
                     class="white--text align-end"
                     height="25vh"
                     src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
                   >
-                    <v-card-title align="end"> Category Name</v-card-title>
-                    <v-card-subtitle> Description of gallery</v-card-subtitle
-                    ><v-card-text> Date information</v-card-text>
-                    <v-row align="center" justify="end"> </v-row>
+                    <v-card-title align="end">Category Name</v-card-title>
+                    <v-card-subtitle>Description of gallery</v-card-subtitle>
+                    <v-card-text>Date information</v-card-text>
+                    <v-row align="center" justify="end"></v-row>
                   </v-img>
                 </v-card>
               </v-col>
             </v-row>
           </v-col>
-          <v-col cols="10"> Accepted Members </v-col>
-          <v-col class="pb-lg-16" cols="10" sm="12" lg="10" justify="end">
-            <v-row class="mb-lg-n16" justify="end">
+          <v-col cols="8" sm="10" lg="9" class="pl-8 pl-sm-14 pl-lg-4">Applicants</v-col>
+          <v-col class="pr-16 pr-lg-0" cols="4" sm="2" lg="1">
+            <v-btn text>View All</v-btn>
+          </v-col>
+          <v-col class="pb-lg-16" cols="12" sm="12" lg="10" justify="end">
+            <v-row class="mb-lg-n16 pl-4 pl-sm-10 pl-lg-0 pr-4 pr-sm-10 pr-lg-8" justify="start">
               <v-col
                 justify="end"
-                v-for="n in 5"
+                v-for="n in (displayGridCount/2)"
                 :key="n"
-                cols="10"
-                sm="6"
+                cols="6"
+                sm="3"
+                md="3"
                 lg="3"
                 xl="2"
                 :class="{
-                  custom5cols: $vuetify.breakpoint.smAndUp
+                  custom5cols: $vuetify.breakpoint.mdAndUp
                 }"
               >
                 <v-card style="height:25vh" outlined title>
@@ -111,28 +106,32 @@
                     height="25vh"
                     src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
                   >
-                    <v-card-title align="end"> Category Name</v-card-title>
-                    <v-card-subtitle> Description of gallery</v-card-subtitle
-                    ><v-card-text> Date information</v-card-text>
-                    <v-row align="center" justify="end"> </v-row>
+                    <v-card-title align="end">Category Name</v-card-title>
+                    <v-card-subtitle>Description of gallery</v-card-subtitle>
+                    <v-card-text>Date information</v-card-text>
+                    <v-row align="center" justify="end"></v-row>
                   </v-img>
                 </v-card>
               </v-col>
             </v-row>
           </v-col>
-          <v-col cols="10"> Declined Members </v-col>
-          <v-col class="pb-lg-16" cols="10" sm="12" lg="10" justify="end">
-            <v-row class="mb-lg-n16" justify="end">
+          <v-col cols="8" sm="10" lg="9" class="pl-8 pl-sm-14 pl-lg-4">Applicants</v-col>
+          <v-col class="pr-16 pr-lg-0" cols="4" sm="2" lg="1">
+            <v-btn text>View All</v-btn>
+          </v-col>
+          <v-col class="pb-lg-16" cols="12" sm="12" lg="10" justify="end">
+            <v-row class="mb-lg-n16 pl-4 pl-sm-10 pr-sm-10 pl-lg-0 pr-4 pr-lg-8" justify="start">
               <v-col
                 justify="end"
-                v-for="n in 5"
+                v-for="n in (displayGridCount/2)"
                 :key="n"
-                cols="10"
-                md="6"
+                cols="6"
+                sm="3"
+                md="3"
                 lg="3"
                 xl="2"
                 :class="{
-                  custom5cols: $vuetify.breakpoint.smAndUp
+                  custom5cols: $vuetify.breakpoint.mdAndUp
                 }"
               >
                 <v-card style="height:25vh" outlined title>
@@ -141,10 +140,10 @@
                     height="25vh"
                     src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
                   >
-                    <v-card-title align="end"> Category Name</v-card-title>
-                    <v-card-subtitle> Description of gallery</v-card-subtitle
-                    ><v-card-text> Date information</v-card-text>
-                    <v-row align="center" justify="end"> </v-row>
+                    <v-card-title align="end">Category Name</v-card-title>
+                    <v-card-subtitle>Description of gallery</v-card-subtitle>
+                    <v-card-text>Date information</v-card-text>
+                    <v-row align="center" justify="end"></v-row>
                   </v-img>
                 </v-card>
               </v-col>
@@ -171,8 +170,8 @@ export default {
 
         case "sm":
           return true;
-        // case "xs":
-        //   return true;
+        case "xs":
+          return true;
       }
     },
     displayGridCount() {
@@ -182,7 +181,7 @@ export default {
         case "sm":
           return 8;
         case "xs":
-          return 2;
+          return 4;
         case "lg":
           return 10;
         case "xl":
