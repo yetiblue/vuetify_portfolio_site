add095d049560a516f289f594c7dd0479c9c70ea
yetiblue <thempver@gmail.com>
2021-06-01 13:12:24 -0400

    fiveColGrid and fiveColPage done

--- portfolio/pages/testtables.vue
+++ portfolio/pages/testtables.vue
@@ -8,7 +8,7 @@
             <v-card-title>Name</v-card-title>
             <v-card-subtitle>User</v-card-subtitle>
             <v-btn class="mb-2" elevation="0" block>Dashboard</v-btn>
-            <v-btn class="mb-2" elevation="0" block>Applications</v-btn>
+            <v-btn class="mb-2 pr-2" elevation="0" block>Applications</v-btn>
             <v-btn class="pr-2 mb-1" elevation="0" block>Edit Profile</v-btn>
           </template>
 
@@ -31,79 +31,83 @@
 
         <v-spacer></v-spacer>
       </v-row>
-      <div>
-        <v-row class="pb-lg-16 mb-lg-16" justify="end" align="start">
-          <v-col cols="8" sm="10" lg="9" class="pl-8 pl-sm-14 pl-lg-4">Applicants</v-col>
-          <v-col class="pr-16 pr-lg-0" cols="4" sm="2" lg="1">
-            <v-btn text>View All</v-btn>
-          </v-col>
-          <v-col class="pb-lg-16" cols="12" sm="12" md="12" lg="10" justify="end">
-            <v-row class="mb-lg-n16 pl-4 pr-4 pl-sm-10 pl-lg-0 pr-sm-10 pr-lg-8" justify="start">
-              <v-col
-                justify="end"
-                v-for="n in displayGridCount"
-                :key="n"
-                cols="6"
-                sm="4"
-                md="3"
-                lg="3"
-                xl="2"
-                :class="{
-                  custom5cols: $vuetify.breakpoint.lgAndUp
-                }"
-              >
-                <!-- slot -->
-                <v-card :height="gridHeight" outlined title>
-                  <v-img
-                    class="white--text align-end"
-                    :height="gridHeight"
-                    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                  >
-                    <v-card-title class="mb-n5" align="end">Sound Wrangler</v-card-title>
-                    <v-card-text class="mb-n5">Timmy Zhou</v-card-text>
-                    <v-card-text class="mb-4">Date information</v-card-text>
-                    <v-row align="center" justify="end"></v-row>
-                  </v-img>
-                </v-card>
-                <!-- slot -->
-              </v-col>
-            </v-row>
-          </v-col>
+      <v-row class="mt-lg-n16 pb-lg-16 mb-lg-n16" justify="end" align="start">
+        <v-col class="pt-11 pb-lg-n16 mt-sm-n16 mt-lg-0" cols="12" sm="12" lg="10">
+          <v-col>
+            <v-card flat class="text-center text-lg-left ml-lg-n6">
+              <v-card-title :class="[forIpad ? 'justify-center' : 'none']">PIRATES OF THE CARRIBEAN</v-card-title>
+              <v-card-subtitle>Date Posted</v-card-subtitle>
 
-          <v-col cols="8" sm="10" lg="9" class="pl-8 pl-sm-14 pl-lg-4">Sent Applicants</v-col>
-          <v-col class="pr-16 pr-lg-0" cols="4" sm="2" lg="1">
-            <v-btn text>View All</v-btn>
+              <v-card-text class="pr-lg-10 text-center">
+                fdhsiaudsoxjvdfsdfdsfds fdhsiaudsoxjvdfsdfdsfds
+                fdhsiaudsoxjvdfsdfdsfds fdhsiaudsoxjvdfsdfdsfds
+                fdhsiaudsoxjvdfsdfdsfds fdhsiaudsoxjvdfsdfdsfds
+                fdhsiaudsoxjvdfsdfdsfds fdhsiaudsoxjvdfsdfdsfds
+              </v-card-text>
+            </v-card>
           </v-col>
-          <v-col class="pb-lg-16" cols="12" sm="12" md="12" lg="10" justify="end">
-            <v-row class="mb-lg-n16 pl-4 pr-4 pl-sm-10 pl-lg-0 pr-sm-10 pr-lg-8" justify="start">
-              <v-col
-                justify="end"
-                v-for="n in displayGridCount"
-                :key="n"
-                cols="6"
-                sm="4"
-                md="3"
-                lg="3"
-              >
-                <!-- slot -->
-                <v-card :height="gridHeight" outlined title>
-                  <v-img
-                    class="white--text align-end"
-                    :height="gridHeight"
-                    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                  >
-                    <v-card-title class="mb-n5" align="end">Category Name</v-card-title>
-                    <v-card-text class="mb-n5">Description of gallery</v-card-text>
-                    <v-card-text class="mb-4">Date information</v-card-text>
-                    <v-row align="center" justify="end"></v-row>
-                  </v-img>
-                </v-card>
-                <!-- slot -->
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-      </div>
+        </v-col>
+      </v-row>
+      <fiveColGrid :displayGridCount="displayGridCount">
+        <template #rowTitleOne>Testing Slot</template>
+        <template #navButtonOne>
+          <v-btn text>See All</v-btn>
+        </template>
+        <template #cardSlot>
+          <v-card :height="gridHeight" outlined title>
+            <v-img
+              class="white--text align-end"
+              :height="gridHeight"
+              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            >
+              <v-card-title class="mb-n5" align="end">Category Name</v-card-title>
+              <v-card-text class="mb-n5">Timmy Zhou</v-card-text>
+              <v-card-text class="mb-5">Date information</v-card-text>
+              <v-row align="center" justify="end"></v-row>
+            </v-img>
+          </v-card>
+        </template>
+      </fiveColGrid>
+      <fiveColGrid :displayGridCount="displayGridCount">
+        <template #rowTitleTwo>Second Slot</template>
+        <template #navButtonTwo>
+          <v-btn text>See All</v-btn>
+        </template>
+        <template #cardSlotTwo>
+          <v-card :height="gridHeight" outlined title>
+            <v-img
+              class="white--text align-end"
+              :height="gridHeight"
+              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            >
+              <v-card-title class="mb-n5" align="end">Category Name</v-card-title>
+              <v-card-text class="mb-n5">Timmy Zhou</v-card-text>
+              <v-card-text class="mb-5">Date information</v-card-text>
+              <v-row align="center" justify="end"></v-row>
+            </v-img>
+          </v-card>
+        </template>
+      </fiveColGrid>
+      <fiveColGrid :displayGridCount="displayGridCount">
+        <template #rowTitleThree>Third Slot</template>
+        <template #navButtonThree>
+          <v-btn text>See All</v-btn>
+        </template>
+        <template #cardSlotThree>
+          <v-card :height="gridHeight" outlined title>
+            <v-img
+              class="white--text align-end"
+              :height="gridHeight"
+              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            >
+              <v-card-title class="mb-n5" align="end">Category Name</v-card-title>
+              <v-card-text class="mb-n5">Timmy Zhou</v-card-text>
+              <v-card-text class="mb-5">Date information</v-card-text>
+              <v-row align="center" justify="end"></v-row>
+            </v-img>
+          </v-card>
+        </template>
+      </fiveColGrid>
     </v-app>
     <BottomFooterComponent />
   </div>
@@ -121,7 +125,7 @@ export default {
     fiveColGrid
   },
   data() {
-    return { sideHeight: `20vh`, isUser: true, isDirector: false };
+    return { sideHeight: `10vh`, isUser: true, isDirector: false };
   },
   computed: {
     forIpad() {
