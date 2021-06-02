71aa366e73d4e314d0c8699ed12e0cbc3b427703
yetiblue <thempver@gmail.com>
2021-06-02 11:00:27 -0400

    testing env in gitignore

---
+++ portfolio/components/buttonComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row>
+    <v-col cols="12" sm="4">
+      <v-btn class="grey" elevation="0" block :to="'google.com'">Dashboard</v-btn>
+    </v-col>
+    <v-col cols="12" sm="4">
+      <v-btn elevation="0" block :to="'google.com'">Applications</v-btn>
+    </v-col>
+    <v-col cols="12" sm="4">
+      <v-btn elevation="0" block :to="'google.com'">Edit Profile</v-btn>
+    </v-col>
+  </v-row>
+</template>
